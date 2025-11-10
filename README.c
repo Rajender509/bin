62cd0ee812642a5a6a896cda31ee1015cba25160
Rajender <rajendermadishetti999@gmail.com>
2025-11-10 13:22:12 +0530

    Enhance README with lex and yacc examples
    Updated README to include lexer and parser implementations for variable validation and calculator functionality using lex and yacc.

--- README.c
+++ README.c
@@ -1,95 +1,142 @@
-import 'package:flutter/material.dart';
+%{
+#include "y.tab.h"
+%}
+%%
+[a-zA-Z][a-zA-Z0-9]*    { return VARIABLE; }
+.|\n                    { return yytext[0]; }
 
-void main() {
-  runApp(MyApp());
+%%
+int yywrap()
+{
+    return 1;
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Column Example"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Space evenly between children
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(12.0), // Margin around the container
-              padding: EdgeInsets.all(20.0), // Padding inside the container
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8), // Rounded corners
-                color: Colors.red, // Background color
-              ),
-              child: Text(
-                "Android",
-                style: TextStyle(color: Colors.white, fontSize: 25), // Text style
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(12.0),
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.green,
-              ),
-              child: Text(
-                "iOS",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(12.0),
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.blue,
-              ),
-              child: Text(
-                "Web",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+var.y
+%{
+#include <stdio.h>
+%}
+%token VARIABLE
+
+%%
+input:
+    VARIABLE '\n'     { printf("Valid variable\n"); return 0; }
+  | '\n'              { printf("Empty input\n"); return 0; }
+  ;
+%%
+
+int main() {
+    printf("Enter a variable name:\n");
+    yyparse();
+    return 0;
+}
+int yyerror(const char *msg) {
+    printf("Invalid variable: %s\n", msg);
+    return 0;
 }
 
-cont
-import 'package:flutter/material.dart';
+OUTPUT
+kits@kits-Veriton-M200-B360:~/Desktop$ yacc -d var.y
+kits@kits-Veriton-M200-B360:~/Desktop$ lex var.l
+kits@kits-Veriton-M200-B360:~/Desktop$ gcc lex.yy.c y.tab.c -o varcheck
+kits@kits-Veriton-M200-B360:~/Desktop$ ./varcheck
+Enter a variable name:
+username
+Valid variable
+kits@kits-Veriton-M200-B360:~/Desktop$ ./varcheck
+Enter a variable name:
+*user
+Invalid variable: syntax error
+c) Implementation of calculator using lex and yacc.
 
-void main() => runApp(MyApp());
+calc.l
+%{
+#include "y.tab.h"
+%}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Container Example"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(35), // Padding inside the container
-          margin: EdgeInsets.all(20), // Margin outside the container
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 4), // Black border
-            borderRadius: BorderRadius.circular(8), // Rounded corners
-            boxShadow: [
-              BoxShadow(
-                color: Colors.green, // Shadow color
-                offset: Offset(6.0, 6.0), // Shadow offset
-              ),
-            ],
-          ),
-          child: Text(
-            "Hello! I am in the container widget!",
-            style: TextStyle(fontSize: 30), // Text style
-          ),
-        ),
-      ),
-    );
-  }
+%%
+
+[0-9]+              { yylval = atoi(yytext); return NUMBER; }
+[ \t]               ; // ignore whitespace
+
+\n                  { return NEWLINE; }
+
+"+"                 { return PLUS; }
+"-"                 { return MINUS; }
+"*"                 { return MUL; }
+"/"                 { return DIV; }
+"("                 { return LPAREN; }
+")"                 { return RPAREN; }
+
+.                   { return yytext[0]; }
+
+%%
+
+int yywrap() {
+    return 1;
 }
+
+calc.y
+%{
+#include <stdio.h>
+#include <stdlib.h>
+%}
+
+%token NUMBER
+%token PLUS MINUS MUL DIV LPAREN RPAREN NEWLINE
+
+%left PLUS MINUS
+%left MUL DIV
+%left UMINUS
+
+%%
+
+input:
+      input line
+    | /* empty */
+    ;
+line:
+      expr NEWLINE   { printf("Result = %d\n", $1); }
+    | NEWLINE
+    ;
+expr:
+      expr PLUS expr   { $$ = $1 + $3; }
+    | expr MINUS expr  { $$ = $1 - $3; }
+    | expr MUL expr    { $$ = $1 * $3; }
+    | expr DIV expr    {
+                          if ($3 == 0)
+                              printf("Error: Division by zero!\n");
+                          else
+                              $$ = $1 / $3;
+                       }
+    | LPAREN expr RPAREN { $$ = $2; }
+    | MINUS expr %prec UMINUS { $$ = -$2; }
+    | NUMBER            { $$ = $1; }
+    ;
+
+%%
+
+int main() {
+    printf("Enter expressions (Ctrl+D to exit):\n");
+    yyparse();
+    return 0;
+}
+
+int yyerror(char *msg) {
+    printf("Syntax error!\n");
+    return 1;
+}
+
+OUTPUT
+
+kits@kits-Veriton-M200-B360:~/Desktop$ yacc -d calc.y
+kits@kits-Veriton-M200-B360:~/Desktop$ lex calc.l
+kits@kits-Veriton-M200-B360:~/Desktop$ gcc lex.yy.c y.tab.c -o calculator
+kits@kits-Veriton-M200-B360:~/Desktop$ ./calculator
+Enter expressions (ctrl+d to exit):
+3*4+2
+Result = 14
+3+4*2
+Result = 11
+4%2
+
+
